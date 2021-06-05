bbc18d266f1b698b66394a7d1a9fecbf8b9d7cea
Stefka Vacheva <stef.vacheva@imperiaonline.org>
2021-06-05 14:34:21 +0300

    init base project

---
+++ Memorize/Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Stefka Vacheva on 5.06.21.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
